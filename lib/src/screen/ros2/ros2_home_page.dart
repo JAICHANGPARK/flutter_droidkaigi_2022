89ca94c8dd55109a55e730b877c8465c83fdebcc
Dreamwalker <aristojeff@gmail.com>
2022-09-16 15:36:35 +0900

    Create ros2_home_page.dart

---
+++ lib/src/screen/ros2/ros2_home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+class Ros2HomePage extends StatefulWidget {
+  const Ros2HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<Ros2HomePage> createState() => _Ros2HomePageState();
+}
+
+class _Ros2HomePageState extends State<Ros2HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+
+      ],
+    );
+  }
+}
